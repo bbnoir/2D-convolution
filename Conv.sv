6c686c37e4351c4d6cc4ff0cd3ca506c082c8405
bbnoirz <bbnoir83@gmail.com>
2023-06-21 04:26:34 +0800

    3.6ns/310200/172559

--- Conv.sv
+++ Conv.sv
@@ -15,7 +15,7 @@ module Conv(
 );
 
 //---------------------------------------------------------------------
-//   INPUT AND OUTPUT DECLARATION                         
+//   INPUT AND OUTPUT DECLARATION
 //---------------------------------------------------------------------
 input clk, rst_n, image_valid, filter_valid, filter_size, pad_mode, act_mode;
 input [3:0] image_size;
@@ -24,7 +24,7 @@ output logic out_valid;
 output logic signed [15:0] out_data;
 
 //---------------------------------------------------------------------
-//   Your design                       
+//   Your design
 //---------------------------------------------------------------------
 
 logic signed [7:0] filter [0:24];
@@ -40,8 +40,6 @@ logic [3:0] col, col_nxt;
 logic filter_size_reg, filter_size_nxt;
 logic [3:0] image_size_reg, image_size_nxt;
 logic pad_mode_reg, pad_mode_nxt, act_mode_reg, act_mode_nxt;
-typedef enum logic [1:0] {IDLE, FILT_IN, IMG_IN, OUT} STATE;
-STATE cs, ns;
 logic signed [15:0] mul_result [0:24];
 logic signed [15:0] mul_result_nxt [0:24];
 logic signed [17:0] add_mid [0:6];
@@ -49,9 +47,12 @@ logic signed [17:0] add_mid_nxt [0:6];
 logic signed [20:0] add_result;
 logic signed [20:0] add_result_nxt;
 
+typedef enum logic [1:0] {IDLE, FILT_IN, IMG_IN, OUT} STATE;
+STATE cs, ns;
+
 always_ff @( posedge clk, negedge rst_n ) begin : FSM_FF
 	if ( !rst_n ) cs <= IDLE;
-	else cs <= ns; 
+	else cs <= ns;
 end
 
 always_comb begin : FSM_COMB
@@ -96,6 +97,11 @@ always_comb begin : FILTER_SHIFT_COMB
 		filter_nxt[0] = in_data;
 		for(int i = 1; i < 25; i = i+1) begin
 			filter_nxt[i] = filter[i-1];
+		end
+		if(!filter_size_reg) begin
+			for(int i = 9; i < 25; i = i+1) begin
+				filter_nxt[i] = 0;
+			end
 		end
 	end else begin
 		for(int i = 0; i < 25; i = i+1) begin
@@ -173,7 +179,7 @@ always_comb begin : MUL_OPR_COMB
 		mul_opr_nxt[3] = (col==n-1)? (pad_mode_reg? image[n+1] : 0) : image[n];
 		mul_opr_nxt[5] = (col==0)? (pad_mode_reg? image[n+1] : 0) : image[n+2];
 		mul_opr_nxt[7] = (row==0)? (pad_mode_reg? image[n+1] : 0) : image[2*n+1];
-		mul_opr_nxt[0] = (row==n-1 && col==n-1)? (pad_mode_reg? image[n+1] : 0) : 
+		mul_opr_nxt[0] = (row==n-1 && col==n-1)? (pad_mode_reg? image[n+1] : 0) :
 			(row==n-1)? (pad_mode_reg? image[n] : 0) : (col==n-1)? (pad_mode_reg? image[1] : 0) : image[0];
 		mul_opr_nxt[2] = (row==n-1 && col==0)? (pad_mode_reg? image[n+1] : 0) :
 			(row==n-1)? (pad_mode_reg? image[n+2] : 0) : (col==0)? (pad_mode_reg? image[1] : 0) : image[2];
@@ -187,7 +193,7 @@ always_comb begin : MUL_OPR_COMB
 		mul_opr_nxt[11] = (col==n-1)? (pad_mode_reg? image[2*n+2] : 0) : image[2*n+1];
 		mul_opr_nxt[13] = (col==0)? (pad_mode_reg? image[2*n+2] : 0) : image[2*n+3];
 		mul_opr_nxt[17] = (row==0)? (pad_mode_reg? image[2*n+2] : 0) : image[3*n+2];
-		mul_opr_nxt[6] = (row==n-1 && col==n-1)? (pad_mode_reg? image[2*n+2] : 0) : 
+		mul_opr_nxt[6] = (row==n-1 && col==n-1)? (pad_mode_reg? image[2*n+2] : 0) :
 			(row==n-1)? (pad_mode_reg? image[2*n+1] : 0) : (col==n-1)? (pad_mode_reg? image[n+2] : 0) : image[n+1];
 		mul_opr_nxt[8] = (row==n-1 && col==0)? (pad_mode_reg? image[2*n+2] : 0) :
 			(row==n-1)? (pad_mode_reg? image[2*n+3] : 0) : (col==0)? (pad_mode_reg? image[n+2] : 0) : image[n+3];
@@ -200,21 +206,21 @@ always_comb begin : MUL_OPR_COMB
 		mul_opr_nxt[14] = (col==0)? (pad_mode_reg? image[2*n+2] : 0) : (col==1)? (pad_mode_reg? image[2*n+3] : 0) : image[2*n+4];
 		mul_opr_nxt[22] = (row==0)? (pad_mode_reg? image[2*n+2] : 0) : (row==1)? (pad_mode_reg? image[3*n+2] : 0) : image[4*n+2];
 
-		mul_opr_nxt[1] = (col==n-1)? (pad_mode_reg? mul_opr_nxt[2] : 0) : (row==n-2)? (pad_mode_reg? image[n+1] : 0) : 
+		mul_opr_nxt[1] = (col==n-1)? (pad_mode_reg? mul_opr_nxt[2] : 0) : (row==n-2)? (pad_mode_reg? image[n+1] : 0) :
 			(row==n-1)? (pad_mode_reg? image[2*n+1] : 0) : image[1];
-		mul_opr_nxt[3] = (col==0)? (pad_mode_reg? mul_opr_nxt[2] : 0) : (row==n-2)? (pad_mode_reg? image[n+3] : 0) : 
+		mul_opr_nxt[3] = (col==0)? (pad_mode_reg? mul_opr_nxt[2] : 0) : (row==n-2)? (pad_mode_reg? image[n+3] : 0) :
 			(row==n-1)? (pad_mode_reg? image[2*n+3] : 0) : image[3];
-		mul_opr_nxt[5] = (row==n-1)? (pad_mode_reg? mul_opr_nxt[10] : 0) : (col==n-2)? (pad_mode_reg? image[n+1] : 0) : 
+		mul_opr_nxt[5] = (row==n-1)? (pad_mode_reg? mul_opr_nxt[10] : 0) : (col==n-2)? (pad_mode_reg? image[n+1] : 0) :
 			(col==n-1)? (pad_mode_reg? image[n+2] : 0) : image[n];
-		mul_opr_nxt[15] = (row==0)? (pad_mode_reg? mul_opr_nxt[10] : 0) : (col==n-2)? (pad_mode_reg? image[3*n+1] : 0) : 
+		mul_opr_nxt[15] = (row==0)? (pad_mode_reg? mul_opr_nxt[10] : 0) : (col==n-2)? (pad_mode_reg? image[3*n+1] : 0) :
 			(col==n-1)? (pad_mode_reg? image[3*n+2] : 0) : image[3*n];
-		mul_opr_nxt[21] = (col==n-1)? (pad_mode_reg? mul_opr_nxt[22] : 0) : (row==1)? (pad_mode_reg? image[3*n+1] : 0) : 
+		mul_opr_nxt[21] = (col==n-1)? (pad_mode_reg? mul_opr_nxt[22] : 0) : (row==1)? (pad_mode_reg? image[3*n+1] : 0) :
 			(row==0)? (pad_mode_reg? image[2*n+1] : 0) : image[4*n+1];
-		mul_opr_nxt[23] = (col==0)? (pad_mode_reg? mul_opr_nxt[22] : 0) : (row==1)? (pad_mode_reg? image[3*n+3] : 0) : 
+		mul_opr_nxt[23] = (col==0)? (pad_mode_reg? mul_opr_nxt[22] : 0) : (row==1)? (pad_mode_reg? image[3*n+3] : 0) :
 			(row==0)? (pad_mode_reg? image[2*n+3] : 0) : image[4*n+3];
-		mul_opr_nxt[9] = (row==n-1)? (pad_mode_reg? mul_opr_nxt[14] : 0) : (col==1)? (pad_mode_reg? image[n+3] : 0) : 
+		mul_opr_nxt[9] = (row==n-1)? (pad_mode_reg? mul_opr_nxt[14] : 0) : (col==1)? (pad_mode_reg? image[n+3] : 0) :
 			(col==0)? (pad_mode_reg? image[n+2] : 0) : image[n+4];
-		mul_opr_nxt[19] = (row==0)? (pad_mode_reg? mul_opr_nxt[14] : 0) : (col==1)? (pad_mode_reg? image[3*n+3] : 0) : 
+		mul_opr_nxt[19] = (row==0)? (pad_mode_reg? mul_opr_nxt[14] : 0) : (col==1)? (pad_mode_reg? image[3*n+3] : 0) :
 			(col==0)? (pad_mode_reg? image[3*n+2] : 0) : image[3*n+4];
 
 		mul_opr_nxt[0] = (col >= n-2)? (pad_mode_reg? mul_opr_nxt[1] : 0) :
